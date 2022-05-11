be07fdf21d18902820f4733867c1e34adcb2dcdc
nikihdyt <nikihidayati20@gmail.com>
2022-05-11 23:31:46 +0700

    add homepage UI using FutureBuilder

--- lib/database.dart
+++ lib/database.dart
@@ -10,11 +10,12 @@ class DBHelper {
   // menggunakan pola singleton -> hanya dapat dibuat instancenya sekali
   // me-return database yang dipakai di aplikasi ini
   Future<Database> get dbInstance async {
-    if (_database != null)
+    if (_database != null) {
       return _database;
-
-    _database = await initDB();
-    return _database;
+    } else{
+      _database = await initDB();
+      return _database;
+    }
   }
 
   // membuat instance database
@@ -34,4 +35,22 @@ class DBHelper {
         }
     );
   }
+
+  // me return daftar catatan yang sudah ada
+  Future<List<Note>> getNotes() async {
+    // mengambil referensi dari database
+    final db = await dbInstance;
+
+    // query daftar tabelnya, yg diambil dari nama tabel yg dibuat di initDB() -> onCreate
+    final List<Map<String, dynamic>> maps = await db.query('notes');
+
+    // loop isi data di dalam daftar catatannya
+    return List.generate(maps.length, (index) {
+      // return dalam bentuk maps (maps: var yg dibuat diatas)
+      return Note(id: maps[index]['id'],
+          title: maps[index]['title'],
+          body: maps[index]['body']);
+    });
+  }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/database.dart';
+import 'Note.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,35 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Note App',
+      home: MyHomePage(title: 'Note App Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +29,64 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+  late Future<List<Note>> notes; // variabel daftar catatan
+  var dbHelper;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // buat instance class DBHelper dari database.dart
+    dbHelper = DBHelper();
+
+    loadNotes();
+  }
+
+  // memanggil/load catatannya
+  loadNotes() {
+    // letakkan fungsi di dalam setState karena mereka akan mengubah state/UI nya
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      // beri nilai untuk notes (yg dipanggil di UI)
+      notes = dbHelper.getNotes();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // data yang muncul di UI datangnya di Future, jadi pakai widget FutureBuilder
+      body: FutureBuilder(
+          // data yg diambil di future -> var notes
+          future: notes,
+          builder: (context, AsyncSnapshot snapshot) { // snapshot: panggilan dari data-data di daftar catatan
+            if (snapshot.hasData) {
+              // success
+              if (snapshot.data!.length == 0)
+                return Center(child: Text('still empty'));
+
+              return Container(
+                  child: ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      final currentItem = snapshot.data![index];
+                      return ListTile(title: Text('${currentItem.title}'));
+                    },
+                  ));
+            } else if (snapshot.hasError) {
+              // error
+              return Center(
+                child: Text('error fetching notes'),
+              );
+            } else {
+              // loading
+              return CircularProgressIndicator();
+            }
+          }
+      )
     );
   }
 }
